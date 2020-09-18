5a79e07c807c5f6c951fb34b7318ad1e45b10491
Jeff Ma <jeff-2c@hotmail.com>
2020-09-17 20:57:53 -0400

    Update contact links.

--- src/components/MsgBox/MsgBox.js
+++ src/components/MsgBox/MsgBox.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import MsgBoxSVG from '../../static/img/msgbox.svg';
+import styled from 'styled-components';
 
+// For Message Box SVG; Experimental:
+const StyledMsgBox = styled.div`
+    // background-image: url('${MsgBoxSVG}');
+    // background-repeat: no-repeat;
+    // background-size: cover;
+`;
 
 const MsgBox = (props) => {
     return (
-        <div className="text-box message-box">
+        <StyledMsgBox className="text-box message-box">
             <h1>{props.landCopy.header}</h1>
             <p>{props.landCopy.blurb}</p>
-        </div>
+        </StyledMsgBox>
     );
 }
 

6ab31cb1a918cfe19994dd26060e5f03389f64c9
Jeff Ma <jeff-2c@hotmail.com>
2020-09-17 21:31:43 -0400

    Updates

---
+++ precache-manifest.c8619f02b42536c0cb554704d92bd796.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8973e6d7d03d0de84a4dddd05b0e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "665cc419db3a754f809b",
+    "url": "/static/css/main.3a620e1a.chunk.css"
+  },
+  {
+    "revision": "d1a4b4739132b71e2c3b",
+    "url": "/static/js/2.caca615c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.caca615c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665cc419db3a754f809b",
+    "url": "/static/js/main.32fa5445.chunk.js"
+  },
+  {
+    "revision": "3bea8a48fc20a5c7360f",
+    "url": "/static/js/runtime-main.962635e3.js"
+  },
+  {
+    "revision": "11225e764948bed87adbb2fec96ec580",
+    "url": "/static/media/desktop-profile.11225e76.jpg"
+  },
+  {
+    "revision": "c39ebe1c5794a0f84e01dacfb4326ab4",
+    "url": "/static/media/mobile-profile.c39ebe1c.png"
+  },
+  {
+    "revision": "81f72e189ff85ba9f8819ddee42a2c3d",
+    "url": "/static/media/msgbox.81f72e18.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b084497291056d64f91f147777013b62.js"
+  "/precache-manifest.c8619f02b42536c0cb554704d92bd796.js"
 );
 
 self.addEventListener('message', (event) => {
